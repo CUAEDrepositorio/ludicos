5aa26671487bf3dc345012722aca4e6c1d2cb9d6
Karen <maykaren20ram@gmail.com>
2020-02-26 17:35:31 -0600

    Banderas de temporizador, debug y SCORM

--- memorama/memorama/js/configRecurso.js
+++ memorama/memorama/js/configRecurso.js
@@ -2,7 +2,7 @@
 var vidmaster=false;
 
 //boton rendirse revela las cartas faltantes
-var rendirse=true;
+var rendirse = false;
 //contiene la ruta de la cubierta de las cartas
 var imagentapa="./imagenes/bocina.png";
 
@@ -11,24 +11,30 @@ var anchocarta=105;
 var altocarta=105;
 
 //opcion para separar las cartas o tenerlas en un solo contenedor
-var separador=true;
+var separador=false;
 //idioma ya sea español= ESP ò bien inglès=ENG
 var idioma="ESP";
 
+var activarson = false; //Activar sonido
+var tempo = false; //Activar funcion de temporizador KDMR
+var minutes = 1 ; //establecer el tiempo del temporizador, antes debe estar en TRUE la variable TEMPO KDMR
+var seg = 30;
+var debug = false; //activar el debug
 
 var ambSCORM = false;
 var barraSCORM = false;
+var idObjetivo = 0;
   //para retros en español
-var retroCal1 = [
-	{LimInf: 0, LimSup: 3, Mensaje: "No fue suficiente"},	
-	{LimInf: 4, LimSup: 6, Mensaje: "Esfuérzate más"},	
-	{LimInf: 7, LimSup: 9, Mensaje: "Suficiente"},	
-	{LimInf: 10, LimSup: 10, Mensaje: "Excelente"},	
+var retroCal = [
+    {LimInf: 0, LimSup: 3, Mensaje: "No fue suficiente"},
+    {LimInf: 4, LimSup: 6, Mensaje: "Esfuérzate más"},
+    {LimInf: 7, LimSup: 9, Mensaje: "Suficiente"},
+    {LimInf: 10, LimSup: 10, Mensaje: "Excelente"},
     ];
     //para retros en inglès
     var retroCal2 = [
-        {LimInf: 0, LimSup: 3, Mensaje: "It was not enough"},	
-        {LimInf: 4, LimSup: 6, Mensaje: "Work harder"},	
-        {LimInf: 7, LimSup: 9, Mensaje: "Enough"},	
-        {LimInf: 10, LimSup: 10, Mensaje: "Excelent"},	
+        {LimInf: 0, LimSup: 3, Mensaje: "It was not enough"},
+        {LimInf: 4, LimSup: 6, Mensaje: "Work harder"},
+        {LimInf: 7, LimSup: 9, Mensaje: "Enough"},
+        {LimInf: 10, LimSup: 10, Mensaje: "Excelent"},
         ];
